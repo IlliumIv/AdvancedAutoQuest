6dcb09592d95ebefc54ca108fe2f6e1cd6839b48
IlliumIv <alm.sparta.ao@gmail.com>
2018-09-17 16:40:59 +0500

    Added testing for AddQuests

--- AdvancedAutoQuest/AdvancedAutoQuest.lua
+++ AdvancedAutoQuest/AdvancedAutoQuest.lua
@@ -2,40 +2,22 @@
 --- Globals
 --------------------------------------------------------------------------------
 
-Global( "CID", 0 )
-Global( "BID", 0 )
-Global( "AvatarLocation", nil )
+--Global("CID", 0)
+--Global("BID", 0)
+Global("ZonesTable", {
+    [0] = {"Quest1.sysName", "Quest2.sysName"},
+    [29862] = {"Quest1.sysName", "Quest2.sysName"} -- Царство Стихий
+})
 
 --------------------------------------------------------------------------------
 --- Functions
 --------------------------------------------------------------------------------
 
 function On_EVENT_INTERACTION_STARTED(params)
+    AvatarReturnQuest()
+    AvatarAcceptQuests()
     local zoneInfo = cartographer.GetCurrentZoneInfo()
-    if zoneInfo.zonesMapId == 29862 then
-        LogInfo(zoneInfo)
-    end
-
-    local retQuestList = avatar.GetReturnableQuests()
-    for i, id in pairs(retQuestList) do
-        local ai = avatar.GetQuestReward( id ).alternativeItems
-        if #ai == 0 then
-            avatar.ReturnQuest( id, nil )
-        else
-            for key, val in pairs(ai) do
-                avatar.ReturnQuest( id, val )
-                break
-            end
-        end
-    end
-
-    local availableQuestList = avatar.GetAvailableQuests()
-    for i, id in pairs(availableQuestList) do
-        local qInf = avatar.GetQuestInfo( id )
-        if (not qInf.isLowPriority and not qInf.isRepeatable) or qInf.canBeSkipped then
-            avatar.AcceptQuest( id )
-        end
-    end
+    AvatarAcceptAddQuests (zoneInfo)
 end
 
 function On_EVENT_QUEST_RECEIVED(params)
@@ -45,6 +27,60 @@ function On_EVENT_QUEST_RECEIVED(params)
     end
     if avatar.IsTalking() then
         On_EVENT_INTERACTION_STARTED()
+    end
+end
+
+function AvatarReturnQuest()
+    local retQuestList = avatar.GetReturnableQuests()
+    if retQuestList then
+        for i, id in pairs(retQuestList) do
+            local ai = avatar.GetQuestReward( id ).alternativeItems
+            if #ai == 0 then
+                avatar.ReturnQuest( id, nil )
+            else
+                for key, val in pairs(ai) do
+                    avatar.ReturnQuest( id, val )
+                    break
+                end
+            end
+        end
+    end
+end
+
+function AvatarAcceptQuests()
+    local availableQuestList = avatar.GetAvailableQuests()
+    if availableQuestList then
+        for i, id in pairs(availableQuestList) do
+            local qInf = avatar.GetQuestInfo( id )
+            if (not qInf.isLowPriority and not qInf.isRepeatable) or qInf.canBeSkipped then
+                avatar.AcceptQuest( id )
+            end
+        end
+    end
+end
+
+function AvatarAcceptAddQuests (zoneInfo)
+    local availableQuestList = avatar.GetAvailableQuests()
+    if availableQuestList then
+        for key, value in pairs(ZonesTable) do
+            if zoneInfo.zonesMapId == key then
+                --Отладка для получения информации о дополнительных квестах и текущей зоне
+                LogInfo(zoneInfo)
+                for k, id in pairs(availableQuestList) do
+                    local qInf = avatar.GetQuestInfo( id )
+                    LogInfo(qInf)
+                end
+                --Отладка для получения информации о дополнительных квестах и текущей зоне
+                for i = 1, #value, 1 do
+                    for k, id in pairs(availableQuestList) do
+                        local qInf = avatar.GetQuestInfo( id )
+                        if qInf.sysName == value[i] then
+                            avatar.AcceptQuest( id )
+                        end
+                    end
+                end
+            end
+        end
     end
 end
 
