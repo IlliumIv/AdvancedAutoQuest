2ce2e67eeceee55d34e2a01f9d7f01e01995be0f
Polyakov <polyakov@macroscop.com>
2026-02-02 18:05:12 +0500

    Update libs and fix ExtractWStringFromValuedText

--- Scripts/ScriptLogExt.lua
+++ Scripts/ScriptLogExt.lua
@@ -5,50 +5,50 @@ local LogWarning = common.LogWarning
 local addonName = common.GetAddonName()
 --------------------------------------------------------------------------------
 local function argtostring(arg, tostring)
-    local result, val, flat = {}
-    for i = 1, arg.n do
-        val, flat = arg[i], arg.n > i
-        if type(val) ~= 'string' then
-            val = tostring(val, flat)
-        end
-        result[#result + 1] = val
-    end
-    return result
+  local result, val, flat = {}
+	for i = 1, arg.n do
+		val, flat = arg[i], arg.n > i
+		if type(val) ~= 'string' then
+      val = tostring(val, flat)
+		end
+		result[#result+1] = val
+	end
+	return result
 end
 
 local function advtostring(v)
-    local vtype = type(v)
-    if vtype == 'string' then
-        return v
-    elseif vtype == 'userdata' then
-        return string.format('%s: %s', common.GetApiType(v), string.match(tostring(v), '(0x%x+)'))
-    else
-        return tostring(v)
-    end
+  local vtype = type(v)
+  if vtype == 'string' then
+    return v
+  elseif vtype == 'userdata' then
+    return string.format('%s: %s', common.GetApiType(v), string.match(tostring(v), '(0x%x+)'))
+  else
+    return tostring(v)
+  end
 end
 
 local function divide(s, size, parts)
-    if not parts then parts = {} end
-    if not size then size = #s end
+  if not parts then parts = {} end
+  if not size then size = #s end
 
-    if #s > size then
-        table.insert(parts, string.sub(s, 1, size))
-        return divide(string.sub(s, size + 1), size, parts)
-    else
-        table.insert(parts, s)
-        return parts
-    end
+  if #s > size then
+    table.insert(parts, string.sub(s, 1, size))
+    return divide(string.sub(s, size + 1), size, parts)
+  else
+    table.insert(parts, s)
+    return parts
+  end
 end
 
 local function log(f, ...)
-    local tostring = rawget(_G, 'advtostring') or advtostring
-    local sarg = table.concat(argtostring({n = select('#', ...), ...}, tostring), ' ')
-    local arg = divide(sarg, 45000)
-    local time = common.GetLocalDateTime()
+  local tostring = rawget(_G, 'advtostring') or advtostring
+  local sarg = table.concat(argtostring({n = select('#', ...), ...}, tostring), ' ')
+  local arg = divide(sarg, 45000)
+  local time = common.GetLocalDateTime()
 
-    f(addonName, string.format('[%02d:%02d:%02d.%03d]: ', time.h, time.min, time.s, time.ms), arg[1])
+  f(addonName, string.format('[%02d:%02d:%02d.%03d]: ', time.h, time.min, time.s, time.ms), arg[1])
 
-    for i = 2, #arg do f(addonName, arg[i]) end
+  for i = 2, #arg do f(addonName, arg[i]) end
 end
 --------------------------------------------------------------------------------
 function common.LogInfo(...) log(LogInfo, ...) end
